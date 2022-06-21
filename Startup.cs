28d66efdb6a8983a39942cd3dfb226c5cc19cc65
pajri <aprilio.pajri@gmail.com>
2022-06-21 08:40:25 +0700

    disable https

--- Startup.cs
+++ Startup.cs
@@ -71,9 +71,9 @@ namespace CarSee
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseSwagger();
