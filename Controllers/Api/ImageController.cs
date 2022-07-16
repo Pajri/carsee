28688d6db42b43e0cd58290293b232eec942644c
pajri <aprilio.pajri@gmail.com>
2022-07-16 10:53:25 +0700

    add image controller

---
+++ Controllers/Api/ImageController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarSee.Dtos;
+using CarSee.Utility.StorageProvider;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSee.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {   
+        private IStorageProvider _storageProvider { get; set; }
+
+        public ImageController(IStorageProvider storageProvider)
+        {
+            _storageProvider = storageProvider;
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Get(string fileName)
+        {
+            var getImage = await _storageProvider.Get(fileName);
+            return File(getImage.FileStream, getImage.ContentType);
+        }
+    }
+} 
